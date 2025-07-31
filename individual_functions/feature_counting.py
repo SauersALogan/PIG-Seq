194eac2b8127395ed8a7063699c853e1540ef46a
logan.sauers@manchester.ac.uk <loganasauers@gmail.com>
2025-07-31 14:15:10 +0100

    Added in the logic, test, and basic function for featureCount counting of gene counts within each assembly gtf file. However, I need to integrate paired rather than Cartesian products

--- individual_functions/feature_counting.py
+++ individual_functions/feature_counting.py
@@ -12,13 +12,53 @@ This file contains the function and its tests
 import tempfile
 import os
 import pytest
-import pandas as pd
+import subprocess
 
 # =============================================================================
 # Actual functions to test
 # =============================================================================
-def SAM_parsing():
-    """Filter SAM files"""
+def build_featureCounts (gtf_files, output_path, sam_files, threads, gene, gene_id):
+    """Count SAM file features using GTF annotation"""
+    featureCounts = [
+        "featureCounts",
+        "-T", str(threads),
+        "-t", gene, # The prokka classification of the read, usually default to gene or CDS
+        "-g", gene_id, # The assigned gene_id, used to group features with similar gene_ids 
+        "-a", gtf_files,
+        "-o", output_path,
+        sam_files
+    ]
+    return featureCounts
+
+def execute_feature_counting(sam_file, gtf_file, threads = 2, gene = "CDS", gene_id = "gene_id")
+    count_tables = []
+    sam_base=os.path.basename(sam_file)
+    sam_name=os.path.splitext(sam_base)[0]
+    output_name=sam_name+".txt"
+    output_path=(output_name)
+    Counter = build_featureCounts(gtf_file, output_path, sam_file, threads, gene, gene_id)
+    try:
+        counts = subprocess.run(Counter, check=True)
+        count_tables.append(output_path)
+    except subprocess.CalledProcessError:
+        print("featureCounts failed to process {sam_file}")
+    return(count_tables)
+
+def run_counter(sam_files, gtf_files, threads = 2, gene = "CDS", gene_id = "gene_id")
+    """Runs the counting function imported above on the actual data"""
+    if isinstance(sam_files, str):
+        sam_file = sam_files
+        gtf_file = gtf_files
+        execute_feature_counting(sam_file, gtf_file, threads, gene, gene_id)
+    elif isinstance(sam_files, list):
+        if isinstance(gtf_files, str):
+        gtf_files = gtf_file
+        for sam_file in sam_files:
+            execute_feature_counting(sam_file, gtf_file, threads, gene, gene_id)
+        if isinstance(gtf_files, list):
+            for gtf_file in gtf_files:
+                for sam_file in sam_files:
+                    execture_feature_counting(sam_file, gtf_file, threads, gene, gene_id)
 
 # =============================================================================
 # Create the mock data for testing
@@ -67,19 +107,28 @@ read6:TT 163     Contig6    17929   60      150M    =       18129350      CAGTCT
     return sam_files
 
 @pytest.fixture
-def assembly_to_bin_map():
-"""Create a mock contig to binning map file."""
-    binned_content = """read_001\t100000\t0\t99995\t+\tcontig_1\t750000\t500000\t599995\t99995\t99995\t55
-read_002\t1000\t0\t695\t+\tcontig_2\t375000\t375\t1070\t675\t695\t60
-read_003\t10000\t0\t8995\t+\tcontig_3\t175000\t10000\t18995\t2995\t8995\t54
-read_004\t7000\t3000\t6750\t+\tcontig_4\t55000\t7500\t11250\t3713\t3750\t21"""
-
-    paf_files = []
-    for i, content in enumerate([paf_1_content, paf_2_content], 1):
-        with tempfile.NamedTemporaryFile(mode='w', delete=False, suffix=f'_assembly{i}.paf') as tmp:
-            tmp.write(content)
-            paf_files.append(tmp.name)
-    return paf_files
+def mock_gtf():
+"""Create a mock gtf file for FeatureCounts."""
+    gtf_content = gtf_content = """Contig1	Prokka	gene	1000	2500	.	+	.	gene_id "gene_001"; gene_name "hypothetical_protein_001";
+Contig1	Prokka	CDS	1200	2300	.	+	0	gene_id "gene_001"; gene_name "hypothetical_protein_001";
+Contig1	Prokka	gene	3000	4500	.	-	.	gene_id "gene_002"; gene_name "ABC_transporter";
+Contig1	Prokka	CDS	3200	4300	.	-	0	gene_id "gene_002"; gene_name "ABC_transporter";
+Contig2	Prokka	gene	50000	52000	.	+	.	gene_id "gene_003"; gene_name "ribosomal_protein";
+Contig2	Prokka	CDS	50200	51800	.	+	0	gene_id "gene_003"; gene_name "ribosomal_protein";
+Contig2	Prokka	gene	70000	75000	.	+	.	gene_id "gene_004"; gene_name "DNA_polymerase";
+Contig2	Prokka	CDS	70200	74800	.	+	0	gene_id "gene_004"; gene_name "DNA_polymerase";
+Contig3	Prokka	gene	60000	65000	.	-	.	gene_id "gene_005"; gene_name "membrane_protein";
+Contig3	Prokka	CDS	60200	64800	.	-	0	gene_id "gene_005"; gene_name "membrane_protein";
+Contig3	Prokka	gene	70000	75000	.	+	.	gene_id "gene_006"; gene_name "kinase_domain";
+Contig3	Prokka	CDS	70200	74800	.	+	0	gene_id "gene_006"; gene_name "kinase_domain";
+Contig4	Prokka	gene	75000	80000	.	+	.	gene_id "gene_007"; gene_name "transcriptional_regulator";
+Contig4	Prokka	CDS	75200	79800	.	+	0	gene_id "gene_007"; gene_name "transcriptional_regulator";
+Contig5	Prokka	gene	76000	78000	.	-	.	gene_id "gene_008"; gene_name "oxidoreductase";
+Contig5	Prokka	CDS	76200	77800	.	-	0	gene_id "gene_008"; gene_name "oxidoreductase";
+Contig6	Prokka	gene	15000	20000	.	+	.	gene_id "gene_009"; gene_name "hydrolase";
+Contig6	Prokka	CDS	15200	19800	.	+	0	gene_id "gene_009"; gene_name "hydrolase";
+"""
+    return gtf_content
 
 # =============================================================================
 # Setup the actual tests
